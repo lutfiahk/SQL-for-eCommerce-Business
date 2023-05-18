87d067db7fc76f5abeeaa0cc16010990bf0a67c4
lutfiahusnakhoirunnisa <lutfiahusnakhoirunnisa@gmail.com>
2023-05-18 15:55:39 +0700

    update query

--- 1. CREATE TABLE AND ERD.sql
+++ 1. CREATE TABLE AND ERD.sql
@@ -75,9 +75,6 @@ CREATE TABLE seller_dataset(
 ALTER TABLE order_items_dataset ADD FOREIGN KEY (product_id) REFERENCES product_dataset (product_id) 
 	,ADD FOREIGN KEY (seller_id) REFERENCES seller_dataset (seller_id)
 	,ADD FOREIGN KEY (order_id) REFERENCES orders_dataset (order_id);
-	
 ALTER TABLE orders_dataset ADD FOREIGN KEY (customer_id) REFERENCES customers_dataset (customer_id);
-
 ALTER TABLE order_reviews_dataset ADD FOREIGN KEY (order_id) REFERENCES orders_dataset (order_id);
-
 ALTER TABLE order_payments_dataset ADD FOREIGN KEY (order_id) REFERENCES orders_dataset (order_id);
